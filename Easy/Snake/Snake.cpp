fde10a07338c5a6d5937dd70b39a97e051f546dd
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-05-26 15:06:04 +0000

    Colour

--- Easy/Snake/Snake.cpp
+++ Easy/Snake/Snake.cpp
@@ -12,10 +12,11 @@ int main(){
     noecho(); // Don't echo input
     curs_set(0); // Hide the cursor
     nodelay(stdscr, TRUE); // Don't wait for input
+    start_color(); // Initialize colors
+    init_pair(1, COLOR_GREEN, COLOR_BLACK); // Green colour pair
+    init_pair(2, COLOR_RED, COLOR_BLACK); // Red colour pair
     WINDOW* gameWindow = newwin(LINES, COLS, 0, 0); // Create a new window for the game
     refresh();
-    box(gameWindow, 0, 0); // Draw a box around the window
-    wrefresh(gameWindow); // Refresh the window
     snakeClass Snake; // Create a snake object
     foodClass Food(LINES*COLS/100); // Create food. 1% of the screen is food
     // ================================= //
@@ -23,6 +24,9 @@ int main(){
 
 
     // ==========Title screen=========== //
+    wattr_on(gameWindow, A_BOLD, NULL); // Make the text bold
+    wattr_on(gameWindow, COLOR_PAIR(1), NULL); // Make the text green
+    box(gameWindow, 0, 0); // Draw a box around the window
     mvwprintw(gameWindow, LINES/2, COLS/2 - 5, "~~Snake~~"); // Write the game title in the middle of the window
     mvwprintw(gameWindow, LINES/2 + 1, COLS/2 - 10, "Use W,A,S,D to move"); // Write instructions
     mvwprintw(gameWindow, LINES/2 + 2, COLS/2 - 11, "Press any key to start");
@@ -30,6 +34,8 @@ int main(){
     nodelay(stdscr, FALSE); // Wait for input
     int input = getch();
     nodelay(stdscr, TRUE); // Dont wait for input
+    wattr_off(gameWindow, A_BOLD, NULL); // Turn off bold
+    wattr_off(gameWindow, COLOR_PAIR(1), NULL); // Turn off green
     // ================================= //
 
 
@@ -54,13 +60,16 @@ int main(){
 
     // ==========Game over============== //
     refresh();
+    wattr_on(gameWindow, A_BOLD, NULL); // Turn bold attribute on
+    wattr_on(gameWindow, COLOR_PAIR(2), NULL); // Turn red attribute on
     box(gameWindow, 0, 0);
-    wrefresh(gameWindow);
     mvwprintw(gameWindow, LINES/2, COLS/2 - 5, "Game over!");
     mvwprintw(gameWindow, LINES/2 + 1, COLS/2 - 7, "Your score: %d", Snake.length);
     wrefresh(gameWindow);
     usleep(2000000); // Wait for a bit
     mvwprintw(gameWindow, LINES/2 + 2, COLS/2 - 11, "Press enter to exit...");
+    wattr_off(gameWindow, A_BOLD, NULL); // Turn bold attribute off
+    wattr_off(gameWindow, COLOR_PAIR(2), NULL); // Turn red attribute off
     wrefresh(gameWindow);
     nodelay(stdscr, FALSE);
     input = getch();
