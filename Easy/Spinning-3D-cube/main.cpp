32101f81f5fd82a226b04050e8a237f4645d645f
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-05-27 10:24:15 +0100

    Cleaning up and improving a little bit

--- Easy/Spinning-3D-cube/main.cpp
+++ Easy/Spinning-3D-cube/main.cpp
@@ -10,41 +10,39 @@
 
 
 int main(){
-    // Set up a GLFW window, and init GLAD:
+    // === Set up a GLFW window, and init GLAD ===
     GLFWwindow* window = glInitHelper::setup(); // Setup function exists just to move all the boilerplate crap out of sight
 
-    // === Defining the geometry and create buffers ===
-    // Triangle verts
-    float vertices[] = {
+    // === Defining the geometry ===
+    float vertices[] = { // This is a triangle
         -0.5f, -0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
          0.0f,  0.5f, 0.0f
     };
 
-    // Vertex array object
-    // The vertex array object (VAO) stores all of the state needed to supply vertex data
-    VAO vao;
-
-    // Set up VBO, VBO layout, IBO, VAO
+    // === Set up VBO, VBO layout, IBO, VAO ===
     VBO vertexBuffer(vertices, 3 * 3 * sizeof(float));
-    VBO_layout layout;
+    VBO_layout layout; // This is a struct that defines the layout of the VBO
     layout.pushFloat(3); // Position data
     VAO vertexArray;
     vertexArray.addBuffer(vertexBuffer, layout);
 
-    // Create shader program
+    // === Create shader program ===
     shaderClass shader("GLSL/shader.vert.glsl", "GLSL/shader.frag.glsl");
 
     // === Main loop === //
     while (!glfwWindowShouldClose(window)){
-        // Render
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        // Unbind stuff:
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
 
-        // Use shader
-        shader.use();
-        // Draw the triangle
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Set the background color
+        glClear(GL_COLOR_BUFFER_BIT); // Clear the color buffer
+
+        vertexArray.bind(); // Bind the VAO
+        shader.use(); // Use the shader
+        glDrawArrays(GL_TRIANGLES, 0, 3); // Draw the triangle
 
         // Swap buffers
         glfwSwapBuffers(window);
