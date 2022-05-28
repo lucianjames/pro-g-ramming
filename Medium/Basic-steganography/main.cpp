308b5bb8ff4901c6c8ddc87c27edf1b6bf589b97
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-05-28 15:45:15 +0100

    Lots more err checking and couts and stuff

--- Medium/Basic-steganography/main.cpp
+++ Medium/Basic-steganography/main.cpp
@@ -4,6 +4,7 @@
 #include <bitset>
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 
 void display_image(cv::Mat image){
     cv::namedWindow("Display Image", cv::WINDOW_NORMAL);
@@ -54,20 +55,23 @@ cv::Mat hideDataInImage(std::string targetPath, std::string dataPath){
     std::vector<std::bitset<8>> toHideBits = readBinaryFile(dataPath);
 
     // Load target image
+    std::cout << "Loading target image..." << std::endl;
     cv::Mat targetImage = cv::imread(targetPath);
 
     // Convert target image to bits
+    std::cout << "Converting target image cv::Mat to bitsets..." << std::endl;
     std::vector<std::bitset<8>> targetImageBits = getImageBits(targetImage);
 
     std::cout << "Number of LSBs to hide in: " << targetImageBits.size() << std::endl;
     std::cout << "Hidden data length: " << toHideBits.size() << std::endl;
     if(toHideBits.size() > targetImageBits.size()){
-        std::cout << "Error: Data too big for image...." << std::endl;
-        std::cout << "Returning original image" << std::endl;
+        std::cout << "Error: Data too big for image!" << std::endl;
+        std::cout << "!!!!Returning original image!!!!" << std::endl;
         return targetImage;
     }
     
     // Add the length of toHideBits*8 to the first few LSB bits of each pixel
+    std::cout << "Adding hidden metadata..." << std::endl;
     std::bitset<32> hiddenDataLenBin = toHideBits.size();
     int count = 0;
     for(int bit=0 ; bit < 32; bit++){
@@ -76,6 +80,7 @@ cv::Mat hideDataInImage(std::string targetPath, std::string dataPath){
     }
 
     // Add the actual data to the image
+    std::cout << "Adding hidden data..." << std::endl;
     for(int i = 0; i < toHideBits.size(); i++){
         for(int bit = 0; bit < 8; bit++){
             targetImageBits[count][0] = toHideBits[i][bit];
@@ -84,6 +89,7 @@ cv::Mat hideDataInImage(std::string targetPath, std::string dataPath){
     }
 
     // Convert the bits back into an image
+    std::cout << "Converting bitsets back into a cv::Mat..." << std::endl;
     cv::Mat imageWithData = bitsToImage(targetImageBits, targetImage.rows, targetImage.cols);
     return imageWithData;
 }
@@ -91,12 +97,14 @@ cv::Mat hideDataInImage(std::string targetPath, std::string dataPath){
 
 void recoverHiddenData(std::string imagePath, std::string outputPath){
     // Load image
+    std::cout << "Loading image..." << std::endl;
     cv::Mat image = cv::imread(imagePath);
 
     // Get the bits from the image
     std::vector<std::bitset<8>> imageBits = getImageBits(image);
 
     // Get the length of the data (read first 32 hidden bits)
+    std::cout << "Reading hidden metadata..." << std::endl;
     std::bitset<32> hiddenDataLenBin;
     for(int bit = 0; bit < 32; bit++){
         hiddenDataLenBin[bit] = imageBits[bit][0];
@@ -105,7 +113,7 @@ void recoverHiddenData(std::string imagePath, std::string outputPath){
     // Get the actual data
     int dataLen = hiddenDataLenBin.to_ulong();
     std::cout << "Hidden data length according to hidden metadata: " << dataLen << std::endl;
-    std::cout << "Binary representation: " << hiddenDataLenBin << std::endl;
+    std::cout << "Reading hidden data from image..." << std::endl;
     std::vector<std::bitset<8>> dataBits;
     dataBits.resize(dataLen);
     int count = 32;
@@ -117,15 +125,17 @@ void recoverHiddenData(std::string imagePath, std::string outputPath){
     }
 
     // Write the bits to a file
+    std::cout << "Writing hidden data to file..." << std::endl;
     std::ofstream file(outputPath, std::ios::binary);
     if(file.is_open()){
         for(int i = 0; i < dataBits.size(); i++){
             file << (char)dataBits[i].to_ulong();
         }
     }
+    std::cout << outputPath << " written successfully!" << std::endl;
 }
 
-void invalidArgs(){
+void invalidArgs(){ // Because writing these couts twice is cringe
     std::cout << "Invalid arguments" << std::endl;
     std::cout << "Usage: ./main --hide <target_image> <data_file> <output_image_path>" << std::endl;
     std::cout << "Usage: ./main --recover <image_with_data> <output_data_path>" << std::endl;
@@ -134,28 +144,71 @@ void invalidArgs(){
 // There are two ways to use this program:
 // ./main --hide <target_image> <data_file> <output_image_path>
 // ./main --recover <image_with_data> <output_data_path>
-
 int main(int argc, char** argv){
+    // Prevent segfault if no arguments are given:
     if(argc < 2){
         invalidArgs();
         return -1;
     }
 
+    // Hide data in an image:
     if(strcmp(argv[1], "--hide") == 0){
+        if(argc != 5){ // Check if the correct number of arguments are given
+            invalidArgs();
+            return -1;
+        }
+        // Verify that targetPath and dataPath are valid paths
+        std::filesystem::path targetPathFSP(argv[2]);
+        std::filesystem::path dataPathFSP(argv[3]);
+        if(!std::filesystem::exists(targetPathFSP)){
+            std::cout << "Error targetPath does not exist" << std::endl;
+            return -1;
+        }
+        if(!std::filesystem::exists(dataPathFSP)){
+            std::cout << "Error: dataPath does not exist" << std::endl;
+            return -1;
+        }
         std::string targetPath = argv[2];
         std::string dataPath = argv[3];
         std::string outputPath = argv[4];
+        // Verify that outputPath ends in .png
+        if(outputPath.length() < 4){
+            std::cout << "Output image path must end in .png" << std::endl;
+            return -1;
+        }
+        if(outputPath.substr(outputPath.size()-4, 4) != ".png"){ // This is a bit hacky, requires above length check to ensure a core dump doesnt happen
+            std::cout << "Output image path must end in .png" << std::endl;
+            return -1;
+        }
         cv::Mat imageWithData = hideDataInImage(targetPath, dataPath);
-        cv::imwrite(outputPath, imageWithData);
+        std::cout << "Writing image with hidden data to " << outputPath << std::endl;
+        if(!cv::imwrite(outputPath, imageWithData)){
+            std::cout << "Error: Could not write image to " << outputPath << std::endl;
+            return -1;
+        }else{
+            std::cout << "Done!" << std::endl;
+        }
     }
+
+    // Recover hidden data from an image:
     else if(strcmp(argv[1], "--recover") == 0){
+        // Verify that imagePath is a valid path:
+        std::filesystem::path imagePathFSP(argv[2]);
+        if(!std::filesystem::exists(imagePathFSP)){
+            std::cout << "Error: imagePath does not exist" << std::endl;
+            return -1;
+        }
+        // Do the recovery!:
         std::string imagePath = argv[2];
         std::string outputPath = argv[3];
         recoverHiddenData(imagePath, outputPath);
     }
+
+    // Invalid arguments:
     else{
         invalidArgs();
         return -1;
     }
+
     return 0;
 }
